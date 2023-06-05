351dc1c4572e5cadf6e59e55b1c73d4af9a1a39f
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-05 11:49:40 -0700

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,11 +7,13 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list->next;
+	listint_t *slow, *fast;
 
 	if (!list)
 		return (0);
 
+	slow = list;
+	fast = list->next;
 	while (fast && slow && fast->next)
 	{
 		if (fast == slow)
