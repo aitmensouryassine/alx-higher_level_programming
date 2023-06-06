a5306bd3cced55a99e9bd30abf6cf2ba90b0ac89
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-06 12:26:56 -0700

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    for i in range(0, len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) < (97 + 26):
+            ascii = ord(str[i]) - (97 - 65)
+        else:
+            ascii = ord(str[i])
+
+        print("{ascii:c}".format(ascii = ascii), end="")
+    print()
