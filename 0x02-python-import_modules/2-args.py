0a772ce03eac2498a2e21935f6251f909a76f551
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-08 04:10:44 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argvlen = len(sys.argv)
+    argvlen = len(sys.argv)
 
-if argvlen == 1:
-    print("0 arguments.")
-else:
-    print(argvlen - 1, "arguments:")
-    for i in range(1, argvlen):
-        print("{}: {}".format(i, sys.argv[i]))
+    if argvlen == 1:
+        print("0 arguments.")
+    else:
+        plural = "" if argvlen <= 2 else "s"
+        print("{} argument{}:".format(argvlen - 1, plural))
+        for i in range(1, argvlen):
+            print("{}: {}".format(i, sys.argv[i]))
