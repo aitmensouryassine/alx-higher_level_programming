99552dae630df17760baa4824ce830eee89a429f
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-08 03:27:08 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-print("{a} + {b} = {c}".format(a=a, b=b, c=add_0.add(a,b)))
+print("{a} + {b} = {c}".format(a=a, b=b, c=add(a, b)))
