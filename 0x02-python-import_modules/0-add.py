ff66c94899f561ac950396751cd3067be5d2d94e
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-08 03:37:20 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__name__":
     from add_0 import add
+
     a = 1
     b = 2
-    print("{a} + {b} = {c}".format(a=a, b=b, c=add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
