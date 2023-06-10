59790f0f6b1f5ed2e45ed1e8965bde2d825913c6
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-10 04:45:29 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentense:
+    if sentence:
         return len(sentence), sentence[0]
     else:
-        return 0, None
+        return len(sentence), None
