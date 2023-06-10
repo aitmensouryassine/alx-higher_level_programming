d869585aa18c5b47be06a1becdbcec32cb3a3022
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-10 03:20:25 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,4 @@ def element_at(my_list, idx):
     if (idx < 0 or idx >= len(my_list)):
         return None
     else:
-        for i, num in enumerate(my_list):
-            if idx == i:
-                print("Element at index {} is {:d}".format(i, num))
+        return my_list[idx]
