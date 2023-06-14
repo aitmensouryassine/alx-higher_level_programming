5cbe662e3e0e2c0e039cfce9913462f8bb175fd2
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-14 05:59:14 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,18 +2,19 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    numerals = {"I": 1, "V": 5, "X": 10,
-                "L": 50, "C": 100, "D": 500, "M": 1000}
-    numerals_keys = list(enumerate(numerals))
+
+    numerals = {"I": 1, "V": 5, "X": 10, "L": 50,
+                "C": 100, "D": 500, "M": 1000}
+    numerals_keys = list(numerals.keys())
     prev = ""
     sum = 0
+
     for letter in roman_string:
-        for i, k in numerals_keys:
+        for i, k in enumerate(numerals_keys):
             if letter == k:
-                sub = 1 if letter == "V" else 2
-                if prev == numerals_keys[i - sub][1]:
-                    sum += numerals[k] - \
-                        (numerals[numerals_keys[i - sub][1]] * 2)
+                sub = 1 if k == "V" else 2
+                if prev == numerals_keys[i - sub]:
+                    sum += numerals[k] - (numerals[numerals_keys[i - sub]] * 2)
                 else:
                     sum += numerals[k]
                     prev = k
