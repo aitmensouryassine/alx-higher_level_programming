e68ed888de4c92542d53c5ac331930f17c1f881f
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-27 07:17:54 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ Creates a square """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Return the square area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ gets the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size of the square """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.area() == 0:
+            print()
+        else:
+            for i in range(0, self.size):
+                for j in range(0, self.size):
+                    print("#", end="")
+                print()
