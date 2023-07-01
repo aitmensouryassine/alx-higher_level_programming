9b292b48cba34d5e50ae42f95b34783a19e488ad
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-01 03:00:21 -0700

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+""" A module for 0x07 project (python: test driven development) """
+
+
 def add_integer(a, b=98):
-    """ A function that adds two integers """
+    """ A function that adds two integers
+    If floats were passed, we cast them to integers
+    If args are not of type int or float we raise TypeError """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
