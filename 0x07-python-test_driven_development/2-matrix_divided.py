0d8d5ab357a2df99729aaeedfeb7f91f1ff8703d
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-01 04:16:22 -0700

    Task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Matrix division Module """
+
+
+def matrix_divided(matrix, div):
+    """ Divides all elements of a matrix """
+
+    for row in matrix:
+        if not all((isinstance(col, int) or
+                    isinstance(col, float)) for col in row):
+            raise TypeError("matrix must be a matrix (list of lists)\
+ of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return list(map(lambda row:
+                    list(map(lambda col: round(col/div, 2), row)), matrix))
