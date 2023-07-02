f7e054a4871fec367af12f21f3bc51471c35c405
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-02 05:49:32 -0700

    Task 7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from numpy import matmul
+""" Lazy Matrix Multiplcation Module """
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ multiplies two matrices """
+
+    return matmul(m_a, m_b)
