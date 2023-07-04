0b2c523015cb43ed72d614cc09d9d40822892eab
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-04 05:20:06 -0700

    Task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-magic = []
 def magic_string():
-    magic.append("BestSchool")
-    return ", ".join(magic)
+    magic_string.count = getattr(magic_string, "count", 0) + 1
+    return "BestSchool, " * (magic_string.count - 1) + "BestSchool"
