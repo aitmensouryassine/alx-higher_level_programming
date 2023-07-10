f8b501952d05f847dc8e4deeb2151de7fcc86798
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-10 05:58:20 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Base geometry Module """
+
+
+class BaseGeometry():
+    """ Base geometry class """
+
+    def area(self):
+        raise Exception("area() is not implemented")
