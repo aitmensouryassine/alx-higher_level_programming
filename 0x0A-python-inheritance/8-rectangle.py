a924b8800b15eaaea2c6ea0eb55343e30db62705
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-10 07:23:56 -0700

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(BaseGeometry):
     """ Rectangle class """
 
     def __init__(self, width, height):
-        self.integer_validator(width)
-        self.integer_validator(height)
-        self.width = width
-        self.height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
