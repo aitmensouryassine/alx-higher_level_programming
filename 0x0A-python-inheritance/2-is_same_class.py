f4c500ddb1219897bc17d06c4e0483552dbfca84
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-10 05:26:50 -0700

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+""" Is same class module """
+
+
+def is_same_class(obj, a_class):
+    """ checks if an object is an instance of a class """
+
+    return isinstance(obj, a_class)
