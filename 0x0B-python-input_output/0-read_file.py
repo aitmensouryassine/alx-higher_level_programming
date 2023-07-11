4bbb7708ccd69f7890ada43d08d38f86e969f031
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-11 04:11:59 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ Reads a file and print it to stdout """
 
-    with open(filename, encoding="UTF-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read())
