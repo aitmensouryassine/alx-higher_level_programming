07d8a10b1e1d08952861f3b6a8432a5c22338168
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-11 14:30:19 -0700

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,32 +3,38 @@
 import sys
 
 
-stats = {"size": 0, "200": 0, "301": 0, "400": 0,
-         "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-count = 0
+def print_stats(stats):
+    """ prints stats """
 
-try:
-    for line in sys.stdin:
-        stat = line.split(" ")
-
-        status_code = stat[7]
-        file_size = stat[8]
-
-        stats[status_code] += 1
-        stats["size"] += int(file_size)
-
-        count += 1
-
-        if count == 10:
-            print("File size: {}".format(stats["size"]))
-            for key in status_codes:
-                if stats[key] != 0:
-                    print("{}: {}".format(key, stats[key]))
-            count = 0
-
-except KeyboardInterrupt as err:
     print("File size: {}".format(stats["size"]))
-    for key in status_codes:
-        if stats[key] != 0:
+    for key in sorted(stats):
+        if key != "size" and stats[key] != 0:
             print("{}: {}".format(key, stats[key]))
+
+if __name__ == "__main__":
+    stats = {"size": 0, "200": 0, "301": 0, "400": 0,
+             "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(stats)
+                count = 0
+
+            stat = line.split(" ")
+
+            status_code = stat[7]
+            file_size = stat[8]
+
+            if status_code in stats:
+                stats[status_code] += 1
+            stats["size"] += int(file_size)
+
+            count += 1
+        print_stats(stats)
+
+    except KeyboardInterrupt as err:
+        print_stats(stats)
+        raise
