97888f82ca9b9518339c06f2e37d65dba524c7ea
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-11 11:24:13 -0700

    Task 14

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+
+
+stats = {"size": 0, "200": 0, "301": 0, "400": 0,
+         "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+count = 0
+
+try:
+    for line in sys.stdin:
+        stat = line.split(" ")
+
+        status_code = stat[7]
+        file_size = stat[8]
+
+        stats[status_code] += 1
+        stats["size"] += int(file_size)
+
+        count += 1
+
+        if count == 10:
+            print("File size: {}".format(stats["size"]))
+            for key in stats:
+                if key != "size" and stats[key] != 0:
+                    print("{}: {}".format(key, stats[key]))
+            count = 0
+
+except KeyboardInterrupt as err:
+    print("File size: {}".format(stats["size"]))
+    for key in stats:
+        if key != "size" and stats[key] != 0:
+            print("{}: {}".format(key, stats[key]))
