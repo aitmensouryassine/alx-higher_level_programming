1fd734f88b6dad6ab107953300317de9b9ca1d73
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-11 14:12:47 -0700

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,6 +5,7 @@ import sys
 
 stats = {"size": 0, "200": 0, "301": 0, "400": 0,
          "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
 count = 0
 
 try:
@@ -21,13 +22,13 @@ try:
 
         if count == 10:
             print("File size: {}".format(stats["size"]))
-            for key in stats:
-                if key != "size" and stats[key] != 0:
+            for key in status_codes:
+                if stats[key] != 0:
                     print("{}: {}".format(key, stats[key]))
             count = 0
 
 except KeyboardInterrupt as err:
     print("File size: {}".format(stats["size"]))
-    for key in stats:
-        if key != "size" and stats[key] != 0:
+    for key in status_codes:
+        if stats[key] != 0:
             print("{}: {}".format(key, stats[key]))
