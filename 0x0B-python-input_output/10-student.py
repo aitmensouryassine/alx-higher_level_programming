4f9ba67c8f1a2e52d828551c83d83b46d28c1348
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-11 08:34:19 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student():
+    """ defines a student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs or not all(type(attr) is str for attr in attrs):
+            return self.__dict__
+
+        return {key: self.__dict__[key] for key in attrs
+                if key in self.__dict__}
