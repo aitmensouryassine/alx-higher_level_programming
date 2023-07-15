16ff72ee995fd056d4a59d77ded2d9bf5326e9e4
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-07-15 02:20:28 -0700

    Task 0 & 1

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,22 @@
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(33)
+        self.assertEqual(b3.id, 33)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
