262c47da9687d84ab5abc2e45197b95d085d2c87
HAJAR <elabdellaoui.hajar@gmail.com>
2023-09-13 16:19:45 +0000

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let l = 0; l < this.height; l++) {
+      let row = '';
+      for (let c = 0; c < this.width; c++) row += 'X';
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
