aed781d3d1f5cf6d9849048da160a87947fa0d9a
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-09-15 15:02:23 -0700

    fix task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     mysql_engine = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(mysql_engine.format(USER, PASS, DB))
+    engine = create_engine(mysql_engine.format(argv[1], argv[2], argv[3]))
 
     Session = sessionmaker(bind=engine)
     session = Session()
