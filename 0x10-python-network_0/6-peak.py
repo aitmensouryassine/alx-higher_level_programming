b43070df60b04b4b627722cf8cc06ab1d540b808
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-09-28 16:10:23 -0700

    fix efficient

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,18 +9,12 @@ def find_peak(list_of_integers):
     if list_of_integers == []:
         return None
 
-    length = len(list_of_integers)
-    if length == 1:
-        return list_of_integers[0]
-    elif length == 2:
-        return max(list_of_integers)
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        middle = (left + right) // 2
+        if list_of_integers[middle] > list_of_integers[middle + 1]:
+            right = middle
+        else:
+            left = middle + 1
 
-    middle = int(length / 2)
-    peak = list_of_integers[middle]
-    if peak > list_of_integers[middle - 1] and \
-            peak > list_of_integers[middle + 1]:
-        return peak
-    elif peak < list_of_integers[middle - 1]:
-        return find_peak(list_of_integers[:middle])
-    else:
-        return find_peak(list_of_integers[middle + 1:])
+    return list_of_integers[left]
