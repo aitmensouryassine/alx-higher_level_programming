8814c6e022fb1ee4454e78569749b317f7358f76
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-09-29 13:20:55 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that fetches https://alx-intranet.hbtn.io/status
+by using the package requests
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    resp = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
