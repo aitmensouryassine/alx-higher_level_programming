d3abda48cf993794c5822d12c600f47a7c7b3469
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-24 18:37:30 +0000

    fix async

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,15 +3,27 @@ const request = require('request');
 const filmId = process.argv[2];
 const filmUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
-request(filmUrl, (err, res, body) => {
-  if (err) return;
-
-  const films = JSON.parse(body).results;
-  const film = films.find(film => film.url.endsWith(`/${filmId}/`));
-  film.characters.forEach(characterUrl => {
-    request(characterUrl, (err, res, body) => {
-      if (err) return;
-      console.log(JSON.parse(body).name);
+function asyncRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
     });
   });
+}
+
+request(filmUrl, async (err, res, body) => {
+  if (err) return;
+  const films = JSON.parse(body).results;
+  const film = films.find(film => film.url.endsWith(`/${filmId}/`));
+
+  let requests = [];
+  film.characters.forEach((characterUrl) => {
+    requests.push(asyncRequest(characterUrl));
+  });
+  requests = await Promise.all(requests);
+
+  requests.forEach(character => console.log(character.name));
 });
