3f1cedf3c330511dbe5ffb7490d3f294018d0953
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-24 13:05:49 +0000

    Write to a file in Nodejs

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs');
+const path = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(path, content, { encoding: 'utf-8' }, err => err && console.log(err));
