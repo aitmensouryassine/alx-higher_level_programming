caacff60c92ce794efb70e994b5a63dc9ae7739b
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-11-01 11:36:09 +0100

    Task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	const button = $("#toggle_header");
+	const header = $("header");
+
+	button.click(function () {
+		if (header.attr("class") == "green") {
+			header.addClass("red");
+			header.removeClass("green");
+		} else {
+			header.removeClass("red");
+			header.addClass("green");
+		}
+	});
+});
